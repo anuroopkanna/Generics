2df04c71d0c7596ce325af657ad4d9c18f89b422
anuroopkanna <anuroopindia16@gmail.com>
2023-03-23 01:39:24 +0530

    added:refactored

--- GenericsProgram.java
+++ GenericsProgram.java
@@ -1,31 +1,17 @@
 package com.BridgeLabz.Generics;
 
-public class GenericsProgram {
-    public void FindMaxInt(Integer a, Integer b, Integer c){
-        int max = a;
-        if((b.compareTo(max))>0)
-        {
-            max=b;
-        }
-        if((c.compareTo(max))>0){
-            max=c;
-        }
-        System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
+public class GenericsProgram<T extends  Comparable<T>> {
+    T x,y,z;
+    public GenericsProgram(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
-
-    public void FindMaxFloat(Float a, Float b, Float c){
-        float max = a;
-        if((b.compareTo(max))>0)
-        {
-            max=b;
-        }
-        if((c.compareTo(max))>0){
-            max=c;
-        }
-        System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
+    public T FindMax(){
+        return GenericsProgram.FindMax(x,y,z);
     }
-    public void FindMaxString(String a,String b,String c){
-        String max=a;
+    public static <T extends Comparable<T>>T FindMax(T a, T b, T c){
+        T max=a;
         if((b.compareTo(max))>0)
         {
             max=b;
@@ -34,14 +20,15 @@ public class GenericsProgram {
             max=c;
         }
         System.out.println("Maximum of "+a+ ", "+b+" and "+c+ " is: "+max);
+        return max;
     }
     public static void main(String[] args) {
         System.out.println("***** Welcome to Generics *****");
-        GenericsProgram genericsProgram = new GenericsProgram();
+        //GenericsProgram genericsProgram = new GenericsProgram();
         Integer a=10,b=5,c=3;
         Float d = 10.5F ,e=5.5F,f=3.3F;
         String g="lohith",h="divya",i="carla";
-        System.out.println("****  Integer values  ****");
+      /*  System.out.println("****  Integer values  ****");
         genericsProgram.FindMaxInt(a,b,c);
         genericsProgram.FindMaxInt(b,a,c);
         genericsProgram.FindMaxInt(c,b,a);
@@ -55,6 +42,9 @@ public class GenericsProgram {
         System.out.println("**** String values **** ");
         genericsProgram.FindMaxString(i,h,g);
         genericsProgram.FindMaxString(h,i,g);
-        genericsProgram.FindMaxString(g,h,i);
+        genericsProgram.FindMaxString(g,h,i);*/
+        new GenericsProgram<Integer>(a,b,c).FindMax();
+        new GenericsProgram<Float>(d,e,f).FindMax();
+        new GenericsProgram<String>(g,h,i).FindMax();
     }
 }
