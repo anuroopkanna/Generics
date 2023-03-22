a37f79700a4c86874360afcf830af014510c2cef
anuroopkanna <anuroopindia16@gmail.com>
2023-03-23 00:53:50 +0530

    genericsstarted

---
+++ GenericsProgram.java
@@ -0,0 +1,8 @@
+package com.BridgeLabz.Generics;
+
+public class GenericsProgram {
+    public static void main(String[] args) {
+        System.out.println("***** Welcome to Generics *****");
+    }
+
+}
